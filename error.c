5f51ed725450f667e07ba6baccbf0c9e230c7143
mariam9498 <45569775+mariam9498@users.noreply.github.com>
2019-02-01 13:23:56 +0100

    Add files via upload

---
+++ error.c
@@ -0,0 +1,56 @@
+#include "error.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+//static int nb;
+typedef struct {
+  smError errors[100];
+  int last;
+}erreur;
+erreur queue;
+
+void creer_file_erreur()
+{
+    queue.last = -1;
+}
+
+void afficher_sm_erreurs()
+{
+    if (queue.last+1==0) printf("\t \t \t Vous n'avez AUCUNE erreur semantique !\n\n");
+    else{
+    
+        printf("\t \t \t Vous avez %d erreurs semantiques !\n\n", queue.last + 1);
+        for (int i = 0; i <= queue.last; i++) {
+        printf("\t \t Ligne %d: %s ", queue.errors[i].line, queue.errors[i].name);
+        switch (queue.errors[i].type) {
+        case AlreadyDeclared:
+            puts("est deja declaree\n");
+            break;
+        case NoneInitialized:
+            puts("est mal initialisee\n");
+            break;
+        case PackNotExist:
+            puts("package incorrecte\n");
+            break;
+        }
+       
+        }
+        }
+    }      
+
+int nombre_sm_erreurs()
+{
+    return queue.last + 1;
+}
+
+void creer_sm_erreur(SemanticErrorType er, char* name, int line){
+	queue.last++;
+  queue.errors[queue.last].type = er;
+  queue.errors[queue.last].line = line;
+  queue.errors[queue.last].name = (char *) malloc((strlen(name) + 1) * sizeof(char));
+  strcpy(queue.errors[queue.last].name, name);
+}
+
+
---
+++ error.h
@@ -0,0 +1,32 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef enum
+    {
+        AlreadyDeclared,
+        NoneInitialized,
+         PackNotExist
+    } SemanticErrorType;
+
+ 
+typedef struct 
+    {
+        char* name;
+        int line;
+        SemanticErrorType type;
+
+    }smError;
+//struct smError *tab;  optionnelle (error.c)
+
+//Constructeur
+void creer_file_erreur(); //cree la structure FIFO globale
+void creer_sm_erreur(SemanticErrorType er, char* name, int line); //cree et ajoute une erreur semantique a la structure FIFO
+
+//Getter
+int nombre_sm_erreurs(); //renvoie le nombre d'erreurs
+void afficher_sm_erreurs(); //affiche les erreurs
+
