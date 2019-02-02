dfab730b656ff4d0197798b708f622acab58253a
BengaadOussama <46943760+BengaadOussama@users.noreply.github.com>
2019-02-02 00:53:32 +0000

    Update error.c

--- error.c
+++ error.c
@@ -34,16 +34,9 @@ void afficher_sm_erreurs()
         case PackNotExist:
             puts("package incorrecte\n");
             break;
-        case NotDeclared:
-            puts(": variable non déclarée\n");
-            break;
-        case IncompatibleForCase:
-            puts("non compatible avec case\n");
-            break;
-        case IntervalError:
-            puts("intervalle non valide\n");
-            break;
-        
+	case SousPackNotExist:
+            puts("sous package incorrecte\n");
+	break;
         }
        
         }
@@ -62,3 +55,5 @@ void creer_sm_erreur(SemanticErrorType er, char* name, int line){
   queue.errors[queue.last].name = (char *) malloc((strlen(name) + 1) * sizeof(char));
   strcpy(queue.errors[queue.last].name, name);
 }
+
+
